ae6909565c7d1146c4c167c6c5ef79f6a2994f5e
Galor Siboni <GalorSiboni@gmail.com>
2020-01-20 23:31:34 +0200

    menu 23:31

--- app/src/main/java/com/example/car/menu.java
+++ app/src/main/java/com/example/car/menu.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class menu extends AppCompatActivity {
 
-    private Button showProfile, showAccidents, insuranceAgent, emergencyServices;
+    private Button showProfile, showAccidents, insuranceAgent, emergencyServices, talkWithClient;
     private String kindOfUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +25,73 @@ public class menu extends AppCompatActivity {
         showAccidents = findViewById(R.id.showAccident);
         insuranceAgent = findViewById(R.id.insuranceAgent);
         emergencyServices = findViewById(R.id.emergency);
+        talkWithClient = findViewById(R.id.talkWithClient);
+
+        visabilitySwitchCase(kindOfUser);
+    }
+
+    private void visabilitySwitchCase(String kind) {
+        switch (kind) {
+            case "user": {
+                talkWithClient.setVisibility(View.GONE);
+                showProfile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        startActivity(new Intent(menu.this,showProfile.class));
+//                        finish();
+                    }
+                });
+                showAccidents.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(menu.this, AccidentReport.class));
+                        finish();
+                    }
+                });
+                insuranceAgent.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        startActivity(new Intent(menu.this,insuranceAgent.class));
+//                        finish();
+                    }
+                });
+                emergencyServices.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        startActivity(new Intent(menu.this,emergencyServices.class));
+//                        finish();
+                    }
+                });
+                break;
+            }
+            case "agent": {
+                showProfile.setVisibility(View.GONE);
+                showAccidents.setVisibility(View.GONE);
+                emergencyServices.setVisibility(View.GONE);
+                insuranceAgent.setVisibility(View.GONE);
+                talkWithClient.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        startActivity(new Intent(menu.this,talkWithClient.class));
+//                        finish();
+                    }
+                });
+                break;
+            }
+            case "witness": {
+                showProfile.setVisibility(View.GONE);
+                showAccidents.setVisibility(View.GONE);
+                insuranceAgent.setVisibility(View.GONE);
+                talkWithClient.setVisibility(View.GONE);
+                emergencyServices.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        startActivity(new Intent(menu.this,emergencyServices.class));
+//                        finish();
+                    }
+                });
+                break;
+            }
+        }
     }
 }
