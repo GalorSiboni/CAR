52db0904f41cf52744146db9b161ce2038657739
Liel Titelbaum <57453032+lieltitelbaum@users.noreply.github.com>
2020-01-20 22:07:25 +0200

    updated accident report manager

--- app/src/main/java/com/example/car/AccidentReport.java
+++ app/src/main/java/com/example/car/AccidentReport.java
@@ -3,12 +3,37 @@ package com.example.car;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 public class AccidentReport extends AppCompatActivity {
 
+    private boolean table_flg = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accident_report);
+
+    }
+
+    public void collapseTable(View view)
+    {
+        TableLayout table = findViewById(R.id.table);
+        Button switchBtn = findViewById(R.id.switchBtn);
+
+        table.setColumnCollapsed(1,table_flg);
+        table.setColumnCollapsed(2,table_flg);
+
+        if(table_flg)
+        {
+            //close
+            table_flg = false;
+            switchBtn.setText("Show Details");
+        }
+        else{
+            //open
+            switchBtn.setText("Hide Details");
+        }
     }
 }
