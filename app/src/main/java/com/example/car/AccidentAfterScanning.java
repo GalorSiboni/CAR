3abb45128e4c31ec5da6c7242355cedd3c4c4054
Liel Titelbaum <57453032+lieltitelbaum@users.noreply.github.com>
2020-03-14 16:04:39 +0200

    14/3 changes in main activity and qr code

--- app/src/main/java/com/example/car/AccidentAfterScanning.java
+++ app/src/main/java/com/example/car/AccidentAfterScanning.java
@@ -3,12 +3,31 @@ package com.example.car;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AccidentAfterScanning extends AppCompatActivity {
+
+    private Button btnOtherDriverInfo;
+    private TextView date, time, location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accident_after_scanning);
+
+        btnOtherDriverInfo = findViewById(R.id.otherDriverInfoBtn);
+        date = findViewById(R.id.dateTextView);
+        location = findViewById(R.id.locationTextView);
+        time = findViewById(R.id.timeTextView);
+
+        btnOtherDriverInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
